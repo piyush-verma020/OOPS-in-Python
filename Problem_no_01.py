a06b9da1a224bdaa0d3e7e5b6e3e2036aa1bf3dd
Piyush Verma <piyush.vermapv38@gmail.com>
2025-04-08 18:51:21 +0530

    Update Problem_no_01.py

--- Problem_no_01.py
+++ Problem_no_01.py
@@ -3,28 +3,41 @@
 
 class programer:
     company = "Microsoft"
+
     # Constructor to initialize object and collect programmer data
     def __init__(self):
-
-        self.num = int(input("Enter the number of programers you wish to store: "))
+        self.num = int(input("Enter the number of programmers you wish to store: "))
         self.data = {}
-        
+
         for i in range(1, self.num + 1):
-            # Take the name of the programmer
-            self.name = input(f"Enter the name of the {i} programer: ")
-            # Take the identity number of the programmer
-            self.value = input(f"Enter the identity number of {i} programer: ")
-            # Store the name and identity number in the dictionary
-            self.data.update({self.name: self.value})
+            name = input(f"Enter the name of the {i} programmer: ")
+            identity = input(f"Enter the identity number of {i} programmer: ")
+            salary = int(input(f"Enter the salary of {i} programmer: "))
+
+            # Store the data in a nested dictionary
+            self.data[name] = {"id": identity, "salary": salary}
 
     # Method to write the collected data to a file
     def update_info_file(self):
-        # Opens a file in write mode and storing the dictionary values as a string
         with open("microsoft.txt", "w") as f:
-            f.write(str(self.data))  # Note: This only writes it as plain string not in readable JSON format
+            f.write(str(self.data))
 
-# Create an object of the class which will collect the data
+    # Method to get salary of a specific programmer
+    def get_salary(self, name):
+        if name in self.data:
+            return self.data[name]["salary"]
+        else:
+            return "Programmer not found."
+
+# Create an object
 k = programer()
+
+# Access class variable
 print(k.company)
-# Calling the method to write the data to an wxisting file
+
+# Save data to file
 k.update_info_file()
+
+# Example: Get salary
+name_to_search = input("Enter the name of the programmer to get their salary: ")
+print("Salary:", k.get_salary(name_to_search))
